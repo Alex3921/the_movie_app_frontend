b09c3c464b3b92110d52fec0b8bb73a25e70d5f4
alexandru <boncut.alex10@gmail.com>
2021-10-10 21:49:33 -0500

    Add search event listener

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Reloading..')
+const movies = new TmdbApi();
+const searchForm = document.getElementById('search-form')
+const search = document.getElementById('search')
+
+
+searchForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const searchTerm = search.value;
+
+  movies.fetchAndRenderMovies(searchTerm)
+});
