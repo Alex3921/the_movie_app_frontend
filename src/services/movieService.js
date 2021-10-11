58f038493e0daf81630d6ab44388ef86d91196fc
alexandru <boncut.alex10@gmail.com>
2021-10-11 17:47:57 -0500

    Transfer methods from 'tmdbApiService'

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,5 +1,14 @@
 class MovieService {
-  constructor(port) {
-    this.port = port
+  constructor() {
+    this.baseUrl = "http://localhost:3000/api/v1/movies";
+    this.searchUrl = "http://localhost:3000/api/v1/movies/search/";
+  }
+
+  async getMovies() {
+    return await fetch(this.baseUrl).then((resp) => resp.json());
+  }
+
+  async searchMovies(query) {
+    return await fetch(this.searchUrl + query).then((resp) => resp.json());
   }
 }
