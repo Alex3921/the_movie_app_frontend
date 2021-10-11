ebe999caabaf7ca899b84a7606c32fe6585d9503
alexandru <boncut.alex10@gmail.com>
2021-10-10 21:48:27 -0500

    Add method 'searchMovies'

--- src/services/tmdbApiService.js
+++ src/services/tmdbApiService.js
@@ -1,9 +1,14 @@
 class TmdbApiService {
   constructor() {
     this.baseUrl = 'http://localhost:3000/api/v1/tmdb_api'
+    this.searchUrl = 'http://localhost:3000/api/v1/tmdb_api/search/'
   }
 
   getMovies() {
     return fetch(this.baseUrl).then(resp => resp.json())
+  }
+
+  searchMovies(query) {
+    return fetch(this.searchUrl + query).then(resp => resp.json())
   }
 }
