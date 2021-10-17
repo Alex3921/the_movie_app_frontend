6a0993246f96fe9057f08c66c830ce6a8eda9314
alexandru <boncut.alex10@gmail.com>
2021-10-17 14:50:57 -0500

    Refactor fetch and search methods

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -4,11 +4,48 @@ class MovieService {
     this.searchUrl = "http://localhost:3000/api/v1/movies/search/";
   }
 
-  async getMovies() {
-    return await fetch(this.baseUrl).then((resp) => resp.json());
+  async fetchAll() {
+    await fetch(this.baseUrl)
+      .then((resp) => resp.json())
+      .then((data) => Movie.renderMovies(data));
   }
 
-  async searchMovies(query) {
-    return await fetch(this.searchUrl + query).then((resp) => resp.json());
+  async search(query) {
+    await fetch(this.searchUrl + query)
+      .then((resp) => resp.json())
+      .then((data) => Movie.renderMovies(data));
   }
+
+  // createMovie(movieData) {
+  //   const { title, overview, vote_average, poster_path, id } = movieData;
+
+  //   const movieInfo = {
+  //     movie: {
+  //       title: title,
+  //       overview: overview,
+  //       vote_average: vote_average,
+  //       poster_path: poster_path,
+  //       external_id: id,
+  //       reviews_attributes:
+  //         {
+  //           author_name: reviewerName.value,
+  //           content: reviewContent.value,
+  //           stars: reviewRating.value
+  //         }
+  //     },
+  //   };
+
+  //   const configObject = {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       Accept: "application/json",
+  //     },
+  //     body: JSON.stringify(movieInfo),
+  //   };
+
+  //   fetch(this.baseUrl, configObject)
+  //     .then((response) => response.json())
+  //     .then((data) => console.log(data));
+  // }
 }
