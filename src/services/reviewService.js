6342c8fd6635053b07b3d5b8e0259828db2369fc
alexandru <boncut.alex10@gmail.com>
2021-10-17 14:47:57 -0500

    Add method 'createReview'

--- src/services/reviewService.js
+++ src/services/reviewService.js
@@ -2,4 +2,28 @@ class ReviewService {
   constructor() {
     this.baseUrl = "http://localhost:3000/api/v1/reviews";
   }
+
+  createReview() {
+    const reviewData = {
+      review: {
+        author_name: authorName.value,
+        content: content.value,
+        stars: starRating.value,
+        movie_id: movieID,
+      },
+    };
+
+    const configObject = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(reviewData),
+    };
+
+    fetch(this.baseUrl, configObject)
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }
 }
