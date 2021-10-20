5dc6872c5672075a8ae4d2e37d3bcfb9ea080595
alexandru <boncut.alex10@gmail.com>
2021-10-19 19:03:35 -0500

    Refactor variable names

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const movieService = new MovieService();
 const reviewService = new ReviewService();
-movieService.fetchAll();
 const searchForm = document.getElementById("search-form");
+
+movieService.fetchAll();
 
 searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -12,17 +13,15 @@ searchForm.addEventListener("submit", (e) => {
   movieService.search(searchTerm);
 });
 
-const modal = document.getElementById("modal");
+const reviewModal = document.getElementById("modal");
+const detailsModal = document.getElementById("details-modal");
 const modalOverlay = document.getElementById("modal-overlay");
 const reviewForm = document.getElementById("review-form");
 
 function openModal() {
   modalOverlay.classList.add("show");
-  modal.classList.add("show");
+  reviewModal.classList.add("show");
   document.getElementById("review-name").focus();
-
-  const closeBtn = document.getElementById("close-btn");
-  closeBtn.addEventListener("click", closeModal);
 
   modalOverlay.addEventListener("click", closeModal);
   reviewForm.addEventListener("submit", submitReview);
@@ -36,14 +35,16 @@ const submitReview = (e) => {
   starRating = getStarRating();
   movieID = e.currentTarget.dataset.movie_id;
 
-  reviewService.createReview()
+  reviewService.createReview();
 
   closeModal();
 };
 
 const closeModal = () => {
+  detailsModal.classList.remove("show");
+  detailsModal.innerText = "";
   reviewForm.reset();
-  modal.classList.remove("show");
+  reviewModal.classList.remove("show");
   modalOverlay.classList.remove("show");
 };
 
@@ -55,6 +56,7 @@ const getStarRating = () => {
   return selection[0];
 };
 
-function openMovieDetails() {
-  debugger
+const closeBtn = document.getElementsByClassName("close-btn");
+for (let i = 0; i < closeBtn.length; i++) {
+  closeBtn[i].addEventListener("click", closeModal);
 }
