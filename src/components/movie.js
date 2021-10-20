4bf6f06613a9c949685e0a18a679f62512fb1832
alexandru <boncut.alex10@gmail.com>
2021-10-19 19:32:15 -0500

    Add method: renderDetails

--- src/components/movie.js
+++ src/components/movie.js
@@ -1,12 +1,13 @@
 class Movie {
   static imgPath = "https://image.tmdb.org/t/p/w300";
 
-  constructor({ id, title, overview, poster_path, vote_average, external_id }) {
+  constructor({ id, title, overview, poster_path, vote_average, reviews }) {
     this.id = id;
     this.title = title;
     this.overview = overview;
     this.poster_path = poster_path;
     this.vote_average = vote_average;
+    this.reviews = reviews;
   }
 
   static renderMovies(collection) {
@@ -31,7 +32,9 @@ class Movie {
       readMoreBtn.type = "button";
       readMoreBtn.innerText = "Read more";
       readMoreBtn.dataset.movie_id = `${movie.id}`;
-      readMoreBtn.addEventListener("click", openMovieDetails);
+      readMoreBtn.addEventListener("click", () => {
+        movieService.getMovie(readMoreBtn.dataset.movie_id)
+      });
 
       const leaveReviewBtn = document.createElement("button");
       leaveReviewBtn.type = "button";
@@ -53,10 +56,66 @@ class Movie {
     });
   }
 
-  getClassByRate(vote) {
-    if (vote > 8) {
+  static renderDetails(m) {
+    const movie = new this(m);
+
+    const detailsModal = document.getElementById("details-modal");
+    
+    const closeBtn = document.createElement('button')
+    closeBtn.className = "close-btn"
+    closeBtn.innerText = "X"
+    closeBtn.addEventListener('click', closeModal)
+
+    const titleContainer = document.createElement("div");
+    titleContainer.id = "title-container"; 
+    
+    const movieTitle = document.createElement("h3")
+    movieTitle.id = "movie-title"
+    movieTitle.innerText = movie.title;
+    
+    const movieRating = document.createElement("span")
+    movieRating.id = "movie-rating"
+    movieRating.innerText = movie.vote_average;
+    movieRating.classList = movie.getClassByRate()
+    
+    titleContainer.appendChild(movieTitle);
+    titleContainer.appendChild(movieRating);
+
+    const movieOverview = document.createElement("div")
+    movieOverview.id = "movie-overview"
+    movieOverview.innerText = movie.overview;
+
+    detailsModal.appendChild(closeBtn);
+    detailsModal.appendChild(titleContainer);
+    detailsModal.appendChild(movieOverview);
+    
+    const reviews = document.createElement("div");
+    reviews.id = "reviews"
+
+    if(movie.reviews.length > 0) {
+      const reviewsTitle = document.createElement("h3");
+      reviewsTitle.id = "reviews-title"
+      reviewsTitle.innerText = "Reviews"
+      reviews.appendChild(reviewsTitle)
+      
+      movie.reviews.forEach((reviewData) => {
+        const review = new Review(reviewData)
+        reviews.appendChild(review.renderReview())
+      })
+    }
+
+    detailsModal.appendChild(reviews)
+    detailsModal.classList.add("show")
+    modalOverlay.classList.add("show");
+
+    modalOverlay.addEventListener("click", closeModal);
+  }
+
+  getClassByRate() {
+    const rating = this.vote_average
+    if (rating > 8) {
       return "green";
-    } else if (vote >= 5) {
+    } else if (rating >= 5) {
       return "orange";
     } else {
       return "red";
